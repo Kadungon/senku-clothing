7cd33fd54cc9bfc2857b53cd31b3b56b3044b76f
kadungon <vishnuskct@gmail.com>
2022-10-28 22:48:07 +0530

    added product context

---
+++ src/context/products.context.jsx
@@ -0,0 +1,17 @@
+import { useState, createContext } from "react";
+import PRODUCTS from "./../shop-data.json";
+
+const ProductsContext = createContext("");
+
+const ProductsProvider = ({ children }) => {
+  const [products, setProducts] = useState(PRODUCTS);
+  const value = { products };
+
+  return (
+    <ProductsContext.Provider value={value}>
+      {children}
+    </ProductsContext.Provider>
+  );
+};
+
+export { ProductsContext, ProductsProvider };
