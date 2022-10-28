016780657cdcfc0a900b8d1f41adf968cb776d8b
kadungon <vishnuskct@gmail.com>
2022-10-28 22:47:52 +0530

    added shop page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Navigation from "./Routes/navigation/navigation.component";
 import Home from "./Routes/home/home.component";
 import Authentication from "./Routes/authentication/authentication.component";
+import Shop from "./Routes/shop/shop.components";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="auth" element={<Authentication />} />
+        <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
   );
