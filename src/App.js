144b789b291d8142fc837233bcec804cd039758c
kadungon <vishnuskct@gmail.com>
2022-10-30 00:45:51 +0530

    added checkout page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navigation from "./Routes/navigation/navigation.component";
 import Home from "./Routes/home/home.component";
 import Authentication from "./Routes/authentication/authentication.component";
 import Shop from "./Routes/shop/shop.components";
+import Checkout from "./Routes/checkout/checkout.component";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="auth" element={<Authentication />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
   );
