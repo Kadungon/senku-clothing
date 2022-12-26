39b497237846ca08692a32e725457e330b735e58
Kadungon <vishnuskct@gmail.com>
2022-12-26 16:18:53 +0530

    added types

--- src/store/categories/categories.actions.ts
+++ src/store/categories/categories.actions.ts
@@ -3,6 +3,7 @@ import {
   createAction,
   Action,
   ActionWithPayload,
+  withMatcher,
 } from "../../utils/reducer/reducer.utils";
 
 export type FetchCategoriesStart =
@@ -19,13 +20,15 @@ export type CategoryAction =
   | FetchCategoriesStart
   | FetchCategoriesSuccess
   | FetchCategoriesError;
-export const fetchCategoriesStart = () =>
-  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+export const fetchCategoriesStart = withMatcher(() => 
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START)
+);
 
-export const fetchCategoriesSuccess = (
-  categories: Category[]
-): FetchCategoriesSuccess =>
-  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categories);
+export const fetchCategoriesSuccess = withMatcher(
+  (categories: Category[]): FetchCategoriesSuccess =>
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categories)
+);
 
-export const fetchCategoriesError = (error: FetchCategoriesError) =>
-  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_ERROR, error);
+export const fetchCategoriesError = withMatcher((error: FetchCategoriesError) =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_ERROR, error)
+);
