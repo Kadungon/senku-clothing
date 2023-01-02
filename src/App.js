7cbb43a2cb9be326f92715a50dc601743c9af610
Kadungon <vishnuskct@gmail.com>
2023-01-02 14:30:43 +0530

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import { Routes, Route } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
 import { useDispatch } from "react-redux";
-
-import Home from "./routes/home/home.component";
-import Navigation from "./routes/navigation/navigation.component";
-
-import Authentication from "./routes/authentication/authentication.component";
-
-import Checkout from "./routes/checkout/checkout.component";
-
-import Shop from "./routes/shop/shop.components";
-
 import { checkUserSession } from "./store/user/user.action";
+import Spinner from "./components/spinner/spinner.component";
+
+const Home = lazy(() => import("./routes/home/home.component"));
+const Navigation = lazy(() =>
+  import("./routes/navigation/navigation.component")
+);
+const Checkout = lazy(() => import("./routes/checkout/checkout.component"));
+const Shop = lazy(() => import("./routes/shop/shop.components"));
+const Authentication = lazy(() =>
+  import("./routes/authentication/authentication.component")
+);
 
 const App = () => {
   const dispatch = useDispatch();
@@ -20,14 +21,16 @@ const App = () => {
     dispatch(checkUserSession());
   }, [dispatch]);
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
-        <Route path="checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
